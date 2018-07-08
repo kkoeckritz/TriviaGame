fb8e209450c8a410757471cb0c2cd4deb6181bf8
Unknown <kjk464@humboldt.edu>
2018-07-07 23:36:03 -0700

    works "perfectly", needs more questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
 var questions = {
     0: {
-        ask: "First question?",
+        ask: "\"Sorry to bother you - where is the capital of Turkey?\"",
         answers: {
-            a1: "correct",
-            a2: "incorrect",
-            a3: "incorrect"
+            a1: "Istanbul",
+            a2: "Constantinople",
+            a3: "Ankara"
         },
-        correct: "a1"
+        correct: "a3"
     },
     1: {
-        ask: "Second question?",
+        ask: "\"Excuse me - where can I find Hollywood?\"",
         answers: {
-            a1: "Not Right!",
-            a2: "Right",
-            a3: "Not Right..."
+            a1: "Los Angeles",
+            a2: "Milwaukee",
+            a3: "Chicago"
         },
-        correct: "a2"
+        correct: "a1"
     },
     2: {
         ask: "Third question?",
@@ -51,11 +51,14 @@ var game = {
     num_correct: 0,
     num_incorrect: 0,
 
-    startOver: function() {
-        // reset divs; show 'ready, set, go'
+    displayIntro: function() {
+        // reset divs, counters; show 'ready, set, go'
         console.log("startOver called");
         $("#g_question").empty();
-        $("#g_answer").empty();
+        $("#g_answers").empty();
+        $("#g_reset").empty();
+        this.num_correct = 0;
+        this.num_incorrect = 0;
 
         console.log("READY");
         $("#g_timer").html("<h2>READY</h2>");
@@ -72,11 +75,11 @@ var game = {
     displayQuestion: function(question) {
         // show countdown; decrement to 0
         var countdown = 10;
-        $("#g_timer").html(`<h4>${countdown}</h4>`);
+        $("#g_timer").html(`<h3>${countdown}</h3>`);
         var count_interval = setInterval(function() {
             countdown--;
             if (countdown > 0) {
-                $("#g_timer").html(`<h4>${countdown}</h4>`);
+                $("#g_timer").html(`<h3>${countdown}</h3>`);
             }
             else {
                 clearInterval(count_interval);
@@ -86,7 +89,7 @@ var game = {
         // show question, answer choices
         console.log("__________________");
         console.log("displayQuestion called");
-        $("#g_question").html(`<h3>${question.ask}</h3>`);
+        $("#g_question").html(`<h2>${question.ask}</h2>`);
         $("#g_answers").empty();
         for (ans in question.answers) {
             $("<h4/>", {
@@ -96,14 +99,15 @@ var game = {
             }).appendTo("#g_answers");
         }
 
-        // add click listener to generated class
-        $(".gen_answers").one("click.gen_answers", function() {
+        // add click listener to generated class (allow user to change answer)
+        $(".gen_answers").on("click.gen_answers", function() {
             // save clicked id
             game.choice = $(this).attr("id");
             console.log(`choice1: ${game.choice}`);
 
-            // kill listener once clicked
-            $(".gen_answers").off("click.gen_answers");
+            // change color to indicate choice
+            $(".gen_answers").removeClass("selected");
+            $(this).addClass("selected");
         });
     },
     displayAnswer: function(question) {
@@ -111,9 +115,11 @@ var game = {
         console.log("displayAnswer called");
         console.log("------------------");
 
+        // kill listener
+        $(".gen_answers").off("click.gen_answers");        
+
         // evaluate clicked id correctness
         if (this.choice == "none") {
-            $(".gen_answers").off("click.gen_answers");
             $("#g_timer").html("<h3>Too Slow!</h3>");
             this.num_incorrect++;
         }
@@ -127,8 +133,8 @@ var game = {
         }
 
         // compare choice (button id) with correct answer
-        $("#g_question").html("<h3>KEEP QUESTION</h3>");
-        $("#g_answers").html("<h3>DISPLAY CORRECT ANSWER</h3>");
+        $("#g_question").html(`<h2>${question.ask}</h2>`);
+        $("#g_answers").html(`<h3>${question.answers[question.correct]}</h3>`);
 
         // reset choice for next question
         this.choice = "none";
@@ -141,52 +147,69 @@ var game = {
         $("#g_answers")
         .html(`<h2>Correct: ${this.num_correct}</h2>`)
         .append(`<h2>Incorrect: ${this.num_incorrect}</h2>`);
+
+        // show reset button
+        $("<input/>", {
+            type: "button",
+            id: "reset",
+            value: "Try again?"
+        }).appendTo("#g_reset");
+
+        // when user clicks, restart everything
+        $("#reset").one("click.reset", function() {
+            game.playGame();
+
+            $("#reset").off("click.reset");
+        })
+    },
+    playGame: function() {
+        // show intro screen
+        game.displayIntro();
+
+        var q_num = 0;
+
+        // wait 2 seconds (for intro) then show first question, answer
+        setTimeout(function() {
+            game.displayQuestion(questions[q_num]);
+            setTimeout(function() {
+                game.displayAnswer(questions[q_num]);
+                q_num++;
+            }, 10000);
+        
+            // show each further question (indexed by q_num), answer; show score if over index
+            var quiz_interval = setInterval(function() {
+                if (q_num < Object.keys(questions).length) {
+                    console.log(`Q_NUM: ${q_num}`);
+
+                    game.displayQuestion(questions[q_num]);
+        
+                    setTimeout(function() {
+                        game.displayAnswer(questions[q_num]);
+                        q_num++;
+                    }, 10000);
+                }
+                else {
+                    console.log(`DONE: Q_NUM: ${q_num}`);
+        
+                    game.displayScore();
+                    clearInterval(quiz_interval);
+                }
+            }, 12000);
+
+        }, 2000);
     }
 }
 
+// run game when page loads
 $().ready(function() {
     // COUNTER FOR TESTING --------------------
-    var foo = 0;
-    $("#test_count").html(`<p>${foo}</p>`);
-    setInterval(function() {
-        foo++;
-        $("#test_count").html(`<p>${foo}</p>`);
-    }, 1000);
+    // var foo = 0;
+    // $("#test_count").html(`<p>${foo}</p>`);
+    // setInterval(function() {
+    //     foo++;
+    //     $("#test_count").html(`<p>${foo}</p>`);
+    // }, 1000);
     // ----------------------------------------
 
-    // show intro screen
-    game.startOver();
-
-    var q_num = 0;
-
-    // wait 2 seconds (for intro) then show first question, answer
-    setTimeout(function() {
-        game.displayQuestion(questions[q_num]);
-        setTimeout(function() {
-            game.displayAnswer(questions[q_num]);
-            q_num++;
-        }, 10000);
-    
-        // show each further question (indexed by q_num), answer; show score if over index
-        var quiz_interval = setInterval(function() {
-            if (q_num < Object.keys(questions).length) {
-                console.log(`Q_NUM: ${q_num}`);
-
-                game.displayQuestion(questions[q_num]);
-    
-                setTimeout(function() {
-                    game.displayAnswer(questions[q_num]);
-                    q_num++;
-                }, 10000);
-            }
-            else {
-                console.log(`DONE: Q_NUM: ${q_num}`);
-    
-                game.displayScore();
-                clearInterval(quiz_interval);
-                // clearInterval(quest_interval);
-            }
-        }, 12000);
-
-    }, 2000);
+    game.playGame();
 })
